a44e57439ecf01fb14ac7bb381ff937bf9fab10f
callegas <callegas.f@gmail.com>
2017-06-27 13:32:01 -0300

    remove duplicate folder

--- web-programming/system-of-series-evaluation/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['logged'] === false) {
-	header("Location: ./index.php");
-}
-$_SESSION['logged'] = false;
-$_SESSION['login'] = null;
-header("Location: ./index.php");
