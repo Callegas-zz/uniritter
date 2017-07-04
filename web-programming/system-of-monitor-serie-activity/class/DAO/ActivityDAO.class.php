7584498f8523444b37974bd060f3c50b61ebd951
Fellipe Callegas <callegas.f@gmail.com>
2017-07-03 21:21:39 -0300

    Fix Bug :feelsgood:

--- web-programming/system-of-monitor-serie-activity/class/DAO/ActivityDAO.class.php
+++ web-programming/system-of-monitor-serie-activity/class/DAO/ActivityDAO.class.php
@@ -1,31 +1,4 @@
-u<?php<?php
-
-class Connection{
-
-	private $user;
-	private $password;
-	private $dataBase;
-	private $path;
-	private static $pdo;
-
-	public function __construct(){
-		$this->user = "root";
-		$this->password = "";
-		$this->dataBase = "web2";
-		$this->path = "localhost";
-	}
-
-	public function connect(){
-		try{
-			if(is_null(self::$pdo)){
-				self::$pdo = new PDO("mysql:host=".$this->path.";dbname=".$this->dataBase, $this->user, $this->password);
-			}
-			return self::$pdo;
-		}catch(PDOException $e){
-			echo 'Error: '.$e->getMessage();
-		}
-	}
-}
+<?php
 
 include_once "Connection.class.php";
 
